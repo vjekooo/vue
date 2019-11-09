e1887909c7efe60b6d3feeb666d9636a5fde2be9
Vjeko <vjeko.pavicin@gmail.com>
2019-11-09 08:38:55 +0100

    get, post endpoints and simple endpoint validation

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,24 @@ import express from 'express'
 (async () => {
 
     const app = express()
+    app.use(express.json())
+
+    app.get('/', (_req, res) => {
+        res.send('aaaaaa')
+    })
+
+    app.post('/api/contact', (req, res) => {
+
+        const { email, message } = req.body
+        // Will validate properly a bit later
+        if (!email && !message) {
+            res.status(422).send('Error')
+
+            return
+        }
+        
+        res.send({message: 'Your message has been sent'})
+    })
 
     const port = process.env.PORT || 4000
     app.listen(port, () => {
