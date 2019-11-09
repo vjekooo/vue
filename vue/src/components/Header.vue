c4e5d1a9b857bccb3a2abcddb68aad9149136069
Vjeko <vjeko.pavicin@gmail.com>
2019-11-09 09:47:23 +0100

    vue router, home / contact routes

---
+++ vue/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="header">
+		<h1>This is header</h1>
+		<nav class="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/contact">Contact</router-link>
+        </nav>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Header',
+	}
+</script>
+
+<style scoped>
+</style>
